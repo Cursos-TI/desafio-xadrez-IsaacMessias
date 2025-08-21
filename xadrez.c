6d1f4e7281447147e2d4ef435ecc04a76ed99153
IsaacMessias <isaacmmsilva80@gmail.com>
2025-08-21 16:54:20 +0000

    intermediario

--- xadrez.c
+++ xadrez.c
@@ -3,13 +3,14 @@
 int main() {
     char escolha;
     int move = 1;
-
+    
     //apredsentação para o menu interativo
         printf("Bem vindo a movimentação de pecas de xadrez\n");
         printf("Selecione a peça que deseja mover:\n");
         printf("B. Bispo\n");
         printf("R. Rainha\n");
         printf("T. Torre\n");
+        printf("H. Cavalo\n");
         scanf(" %c", &escolha);
 
         switch (escolha)
@@ -19,7 +20,7 @@ int main() {
             printf("Você escolheu o Bispo\n");
             printf("O bispo se move 5 casas na diagonal, para cima e direita:\n");
                 for(move; move <= 5; move++){
-                    printf("Bispo: Direito + Cima\n");
+                    printf("Bispo: Direita + Cima\n");
                 }         
             printf("Bispo termina seu movimento\n");
             break;
@@ -48,6 +49,38 @@ int main() {
             
             printf("Torre termina seu movimento\n");
             break;        
+        
+        case 'H'://para torre usei do-while
+        case 'h':
+                
+            int x, y, z;
+
+            printf("Você escolheu o Cavalo:\n");
+            printf("Esocolha  a direção: 1. Cima - 2. Baixo\n");
+            scanf("%d", &y);
+            printf("Escolha  o movimento finel: 1. Esquerda - 2. Direita\n");
+            scanf("%d", &move);
+
+            for (int x = 1; x <= 1; x++){
+                for(z = 1 ; z <= 2; z++)
+                {                    
+                    if(y == 1){
+                        printf("Cima\n");
+                    }else{                       
+                        printf("Baixo\n");
+                    }
+                    
+                }
+                    if (move == 1){
+                        printf("Esquerda\n");
+                    }else{
+                        printf("Direita\n");
+                    }  
+                                      
+            }
+            printf("Cavalo termina seu movimento\n");
+            break;        
+        
         default:
                 printf("Escolha invalida...\n");
             break;
